c4aab0bf38e169ae0ed174dd088db0950640ae51
Kazuki Ota <kaota@microsoft.com>
2019-07-25 11:13:14 +0900

    done

--- TreeViewScrollSample/MainWindow.xaml.cs
+++ TreeViewScrollSample/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace TreeViewScrollSample
 {
@@ -20,9 +13,62 @@ namespace TreeViewScrollSample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IEnumerable<Person> People { get; } = Enumerable.Range(1, 100)
+            .Select(x => new Person
+            {
+                Name = $"Tanaka {x}",
+                Children = Enumerable.Range(1, 10)
+                    .Select(y => new Person
+                    {
+                        Name = $"Kimura {x}-{y}",
+                    })
+                    .ToArray(),
+            })
+            .ToArray();
+
         public MainWindow()
         {
             InitializeComponent();
+            treeView.ItemsSource = People;
+        }
+
+        private async void ScrollButton_Click(object sender, RoutedEventArgs e)
+        {
+            // ContainersGenerated になるまで待つ
+            Task waitUntilContainersGenerated(TreeViewItem container)
+            {
+                var tcs = new TaskCompletionSource<object>(container);
+                void statusChanged(object _, EventArgs __)
+                {
+                    if (container.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
+                    {
+                        tcs.SetResult(null);
+                        container.ItemContainerGenerator.StatusChanged -= statusChanged;
+                        return;
+                    }
+                }
+
+                container.ItemContainerGenerator.StatusChanged += statusChanged;
+                return tcs.Task;
+            }
+
+            // 一番最後の最後の要素にスクロールする予定
+            var parent = People.Last();
+            var target = parent.Children.Last();
+
+            // ツリービュー直下の最後の要素のTreeViewItemを取得
+            var container = (TreeViewItem)treeView.ItemContainerGenerator.ContainerFromItem(parent);
+            // 開く
+            container.IsExpanded = true;
+            // 子の ItemContainerGenerator のステータスが Generated になるまで待つ
+            if (container.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated)
+            {
+                await waitUntilContainersGenerated(container);
+            }
+
+            // スクロール先を取得してスクロール
+            var targetContainer = (TreeViewItem)container.ItemContainerGenerator.ContainerFromItem(target);
+            targetContainer.BringIntoView();
         }
     }
 }
---
+++ TreeViewScrollSample/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeViewScrollSample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public IEnumerable<Person> Children { get; set; }
+    }
+}
